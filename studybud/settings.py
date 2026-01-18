b7dd85aa4c9a0112c0ae74d5c8084e7469c8d363
raphy100 <raphyegbune@gmail.com>
2026-01-18 23:20:42 +0100

    mordify settings

--- studybud/settings.py
+++ studybud/settings.py
@@ -127,6 +127,7 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_ROOT = BASE_DIR / 'static/images'
+ALLOWED_HOSTS = ['raphy.pythonanywhere.com']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
